1b4bd84782c8143c705c9e09e88574a1ca6167a6
tuguiartiom <134213833+tuguiartiom@users.noreply.github.com>
2023-05-21 17:33:04 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const { resolve } = require("path");
+// Replace if using a different env file or config
+const env = require("dotenv").config({ path: "./.env" });
+const cors = require("cors");
+const stripe = require("stripe")(process.env.SECRET, {
+  apiVersion: "2022-08-01",
+});
+
+const corsOpts = {
+  origin: "*",
+
+  methods: ["GET", "POST"],
+
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOpts));
+
+app.get("/", (req, res) => {});
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.PUBLIC,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "USD",
+      amount: req.query.amount,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
